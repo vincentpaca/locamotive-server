c1979d420de88ab1a920ccd4bd8d276b17ddae8d
Vincent Paca <vincent.paca@gmail.com>
2013-02-08 17:54:25 +0800

    Added initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locamotive::Application.config.secret_token = 'cb7221d8bd04861c663e710f3a293a2dd1f0637ff2dabb5653b5758cd7b2c2db19816c1d77cca978d25afd088079237f00ae021a1fd5aa45112d48e5a3a4bd05'
