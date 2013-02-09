aeca7cdb483ee57848aac9ef1a2af135b568de04
Vincent Paca <vincent.paca@gmail.com>
2013-02-09 16:15:45 +0800

    Added restaurant and type relations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,5 @@
 class Restaurant < ActiveRecord::Base
   attr_accessible :address, :delivery, :name, :restaurant_type, :round_the_clock
+
+  belongs_to :restaurant_type
 end
--- app/models/restaurant_type.rb
+++ app/models/restaurant_type.rb
@@ -1,3 +1,4 @@
 class RestaurantType < ActiveRecord::Base
   attr_accessible :name
+  has_many :restaurants
 end
